4055ae2717a959ec056586f9c601dfcb96649694
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-07-11 23:55:01 -0700

    Run timer on app start using factory

--- client/app/app.js
+++ client/app/app.js
@@ -1,26 +1,26 @@
 angular.module('blitzkeys', ['ngSanitize'])
-.controller('gameController', function($scope) {
+.controller('gameController', function($scope, timer) {
   $scope.input = '';
-  // $scope.text = 'Our Goal: You\'ll begin';
-  $scope.text = 'Our Goal: You\'ll begin Hack Reactor with a feeling of excitement and anticipation. Twelve weeks later, you\'ll follow the footsteps of our trailblazing alumni, taking the methodologies and best practices you perfected at our coding bootcamp to your next job. We\'ve built world class software engineering curriculum and programming courses. However, Hack Reactor is, above all else, a world-class learning environment.';
+  $scope.text = 'Our Goal: You\'ll begin';
+  // $scope.text = 'Our Goal: You\'ll begin Hack Reactor with a feeling of excitement and anticipation. Twelve weeks later, you\'ll follow the footsteps of our trailblazing alumni, taking the methodologies and best practices you perfected at our coding bootcamp to your next job. We\'ve built world class software engineering curriculum and programming courses. However, Hack Reactor is, above all else, a world-class learning environment.';
   $scope.start = 0;
   $scope.end = $scope.text.indexOf(' ') + 1;
+  $scope.startTimer = timer.setTimer;
+  $scope.getTimer = timer.getTimer;
+  $scope.stopTimer = timer.stopTimer;
 
   $scope.matchText = function() {
     if ($scope.input === ' ' && $scope.text[$scope.start - 2] === '.') {
       $scope.input = '';
       return;
     }
-    // console.log($scope.text.substring($scope.start, $scope.end), $scope.input.length);
     if ($scope.text.substring($scope.start, $scope.end) === $scope.input) {
-      // console.log('match!', $scope.text.substring($scope.start, $scope.end), $scope.input.length);
       $scope.start = $scope.end;
       if ($scope.text.indexOf(' ', $scope.start) > -1) {
         $scope.end = $scope.text.indexOf(' ', $scope.start) + 1;
       } else {
         $scope.end = $scope.text.length;
       }
-      // console.log($scope.start, $scope.end);
       $scope.input = ''
     }
 
@@ -32,46 +32,53 @@ angular.module('blitzkeys', ['ngSanitize'])
   $scope.highlight = function(start, end) {
     var color = $scope.getColor();
     var full = $scope.text;
-    // console.log('full', full, 'start', $scope.start, 'end', $scope.end);
     var pre = full.substring(0, start);
     var mid = full.substring(start, $scope.end < $scope.text.length ? $scope.end - 1 : $scope.text.length);
     var post = full.substring($scope.end < $scope.text.length ? $scope.end - 1 : $scope.text.length);
-    // console.log('pre', pre, 'mid', mid, 'post', post);
-    // console.log(pre, mid, post);
-    // console.log('return', '<p>' + pre + '<span class="highlighted">' + mid + '</span>' + post + '</p>')
     return '<p>' + pre + '<span class=\" + ' + color + '\">' + mid + '</span>' + post + '</p>';
   }
 
   $scope.getColor = function() {
     var lengthIn = $scope.input.length;
     if (lengthIn > $scope.end - $scope.start) {
-      console.log('length');
       return 'red';
     } else if ($scope.text.substring($scope.start, $scope.start + lengthIn) !== $scope.input) {
-      console.log('err', $scope.text.substring($scope.start, $scope.start + lengthIn), $scope.input);
       return 'red';
     } else {
-      console.log('right');
       return 'green';
     }    
   }
-});
+})
+.factory('timer', function() {
+  var initTime, currentTime, running;
 
-// }).factory('matchFactory', function() {
-//   var text;
-//   var input = '';
+  var startTimer = function() {
+    initTime = Date.now();
+    running = true;
+  };
 
-//   var matchText = function(text, start, end, input) {
-//     if (text.substring(start, end) === input) {
-//       console.log('match!');
-//     }
-//   }
+  var getTimer = function() {
+    return Math.floor((Date.now() - initTime) / 1000);
+  };
 
-//   var getInput = function() {
-//     return input;
-//   }
+  var stopTimer = function() {
+    if (running) {
+      currentTime = Date.now() - initTime;
+      running = false;
+    }
+    return currentTime;
+  };
 
-//   var setInput = function(wordIn) {
-//     input = wordIn;
-//   }
-// })
+  return {
+    startTimer: startTimer,
+    getTimer: getTimer,
+    stopTimer: stopTimer
+  };
+})
+.run(['$rootScope', '$interval', 'timer', function($rootScope, $interval, timer) {
+  timer.startTimer();
+  $rootScope.time = timer.getTimer();
+  var timeIntervalId =$interval(function() {
+    $rootScope.time = timer.getTimer();
+  }, 1000);
+}])
